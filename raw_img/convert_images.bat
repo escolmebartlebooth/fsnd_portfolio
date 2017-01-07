8e01d7ca81246f0c0bbc9b28257bbcf24f13184e
escolmebartlebooth <escolme@bartlebooth.co.uk>
2017-01-07 10:31:36 +0000

    first commit

---
+++ raw_img/convert_images.bat
@@ -0,0 +1,23 @@
+magick convert Project1.jpg -resize 2400x1200 ../img/Project1_2400.jpg
+magick convert Project1.jpg -resize 1200x600 ../img/Project1_1200.jpg
+magick convert Project1.jpg -resize 1800x900 ../img/Project1_1800.jpg
+magick convert Project1.jpg -resize 900x450 ../img/Project1_900.jpg
+magick convert Project1.jpg -resize 600x300 ../img/Project1_600.jpg
+
+magick convert Project2.jpg -resize 2400x1200 ../img/Project2_2400.jpg
+magick convert Project2.jpg -resize 1200x600 ../img/Project2_1200.jpg
+magick convert Project2.jpg -resize 1800x900 ../img/Project2_1800.jpg
+magick convert Project2.jpg -resize 900x450 ../img/Project2_900.jpg
+magick convert Project2.jpg -resize 600x300 ../img/Project2_600.jpg
+
+magick convert Project3.jpg -resize 2400x1200 ../img/Project3_2400.jpg
+magick convert Project3.jpg -resize 1200x600 ../img/Project3_1200.jpg
+magick convert Project3.jpg -resize 1800x900 ../img/Project3_1800.jpg
+magick convert Project3.jpg -resize 900x450 ../img/Project3_900.jpg
+magick convert Project3.jpg -resize 600x300 ../img/Project3_600.jpg
+
+magick convert hero.jpg -resize 2400x1200 ../img/hero_2400.jpg
+magick convert hero.jpg -resize 1200x600 ../img/hero_1200.jpg
+magick convert hero.jpg -resize 1800x900 ../img/hero_1800.jpg
+magick convert hero.jpg -resize 900x450 ../img/hero_900.jpg
+magick convert hero.jpg -resize 600x300 ../img/hero_600.jpg
